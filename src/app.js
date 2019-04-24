65529cbe6b1fd9c1fc10b6fea2f61a02b6bfc0e5
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-23 17:10:15 -0700

    declare variables and given them value of the analogous inputs

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById ('submit-button');
+const biker = document.getElementById ('biker');
+const numStates = document.getElementById ('num-states');
+const hobby = document.getElementById ('hobby');
+const place = document.getElementById ('place');
+const age = document.getElementById ('age');
+const quizResult = document.getElementById ('quiz-result');
