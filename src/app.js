8864ca2a49ee31f6af4a8e482ecf2040016357f2
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-23 17:22:03 -0700

    made the callback function, declared variables and set them to value of original variables that got the value of the inputs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+
 const submitButton = document.getElementById ('submit-button');
 const biker = document.getElementById ('biker');
 const numStates = document.getElementById ('num-states');
@@ -5,3 +7,13 @@ const hobby = document.getElementById ('hobby');
 const place = document.getElementById ('place');
 const age = document.getElementById ('age');
 const quizResult = document.getElementById ('quiz-result');
+
+submitButton.addEventListener('click', () => {
+    const bikerInput = biker.value;
+    const numStatesInput = numStates.value;
+    const hobbyInput = hobby.value;
+    const placeInput = place.value;
+    const ageInput = age.value;
+
+    console.log(bikerInput, numStatesInput, hobbyInput, placeInput, ageInput);
+});
